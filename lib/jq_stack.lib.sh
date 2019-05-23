e92cb6796a69ef1a5a3658abed5910eb03a31412
TsT <tst2005@gmail.com>
2019-05-23 19:17:48 +0200

    be able to use multiple jq_stack commands in one call

--- lib/jq_stack.lib.sh
+++ lib/jq_stack.lib.sh
@@ -1,12 +1,36 @@
 
 jq_stack() {
-	case "$1" in
-	(init)		jq_stack_options='';jq_stack_functions='';jq_stack_calls='.'	;;
-	(run)		jq $jq_stack_options "$jq_stack_functions$jq_stack_calls"	;;
-	(call)		jq_stack_calls="$jq_stack_calls"'|'"$2"				;;
-	(option)	jq_stack_options="$jq_stack_options $2"				;;
-	(function)	jq_stack_functions="$jq_stack_functions$2"			;;
-	(locals)	echo "jq_stack_options jq_stack_functions jq_stack_calls"	;;
-	(*)		echo >&2 "ERROR: jq_stack: Invalid argument #1"; return 1	;;
-	esac
+	while [ $# -gt 0 ]; do
+		case "$1" in
+		(init)
+			jq_stack_options='';jq_stack_functions='';jq_stack_calls=''
+		;;
+		(call)
+			if [ -z "$jq_stack_calls" ]; then
+				jq_stack_calls="$2";shift
+			else
+				jq_stack_calls="$jq_stack_calls"'|'"$2";shift
+			fi
+		;;
+		(option)
+			jq_stack_options="$jq_stack_options $2";shift
+		;;
+		(function)
+			jq_stack_functions="$jq_stack_functions$2";shift
+		;;
+		(locals)
+			echo "jq_stack_options jq_stack_functions jq_stack_calls"
+			return 0
+		;;
+		(run)
+			jq $jq_stack_options "$jq_stack_functions$jq_stack_calls"
+			return $?
+		;;
+		(*)
+			echo >&2 "ERROR: jq_stack: Invalid argument #1";
+			return 1
+		;;
+		esac
+		shift
+	done
 }
