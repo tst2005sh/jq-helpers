49e2e0bdf56fd8420be9e897f3485cb121beefd5
TsT <tst2005@gmail.com>
2020-01-30 19:48:34 +0100

    support all jq 1.5 options

--- lib/jq_stack_oneline.lib.sh
+++ lib/jq_stack_oneline.lib.sh
@@ -4,13 +4,22 @@ jq_stack_oneline() {
 	while [ $# -gt 0 ]; do
 		case "$1" in
 			(-n) dryrun='-n';;
+			(-1) jq_stack option "$2";shift;;
 			(-2) for opt in "$2" "$3"     ; do jq_stack option "$opt"; done; shift 2;;
 			(-3) for opt in "$2" "$3" "$4"; do jq_stack option "$opt"; done; shift 3;;
-			(-f|-L|--f*|--arg*|--indent|--slurp*|--run*)
-				echo >&2 "ERROR: please use -2 $1 ... or -3 $1 ... ..."
+			(-[sRncCMaSrje]|--version|--seq|--stream|--slurp|--raw-input|--null-input|--compact-output|--tab|--color-output|--monochrome-output|--ascii-output|--unbuffered|--sort-keys|--raw-output|--join-output|--exit-status)
+				jq_stack option "$1"
+			;;
+			(--indent|-f|--from-file|-L) # 1 arg
+				for opt in "$1" "$2"; do jq_stack option "$opt"; done; shift
+			;;
+			(--arg|--argjson|--slurpfile|--argfile) # 2 args
+				for opt in "$1" "$2" "$3"; do jq_stack option "$opt"; done; shift 2
+			;;
+			(-*)
+				echo >&2 "ERROR: please use -1 $1 or -2 $1 arg2 or -3 $1 arg2 arg3"
 				return 1
 			;;
-			(-*) jq_stack option "$1" ;;
 			(*:)
 				case "$1" in
 				(*'('*)	# foo(...):
