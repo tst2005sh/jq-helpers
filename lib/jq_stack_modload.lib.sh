9c6006824b587e19adbe579be6589eb18ae24cbf
TsT <tst2005@gmail.com>
2021-03-16 13:51:39 +0100

    use lib/*.jq.lib.sh instead of lib/jq.*.lib.sh

--- lib/jq_stack_modload.lib.sh
+++ lib/jq_stack_modload.lib.sh
@@ -6,7 +6,7 @@ jq_stack_modload() {
 		echo >&2 "No such dir $dir"
 		return 1
 	fi
-	. "$dir/jq.$fname.lib.sh"
+	. "$dir/$fname.jq.lib.sh"
 	if ! eval "test -n \"\${$vname}\""; then
                 echo >&2 "ERROR: $vname not available in env"
 		return 1
