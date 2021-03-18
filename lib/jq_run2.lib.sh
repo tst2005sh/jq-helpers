a4388042ba1c4fb480ba58363f87266e9b23e149
TsT <tst2005@gmail.com>
2021-03-19 00:04:03 +0100

    jq_run2 and jq_gen2

--- lib/jq_run2.lib.sh
+++ lib/jq_run2.lib.sh
@@ -1,6 +1,6 @@
 
-jq_run2_with_prefix() {
-	eval "jq $(
+
+jq_gen2_with_prefix() {
 	{
 	for var in $(jq_stack2 locals); do local "$var"; done
 	local prefix="$1";shift
@@ -9,8 +9,14 @@ jq_run2_with_prefix() {
 		"$prefix$cmd"
 	done
 	} | jq_stack2 run
-	)"
+}
+
+jq_run2_with_prefix() {
+	eval "jq $(jq_gen2_with_prefix "$@")"
 }
 jq_run2() {
 	jq_run2_with_prefix jq_cmd2_ "$@"
+}
+jq_gen2() {
+	jq_gen2_with_prefix jq_cmd2_ "$@"
 }
